100a2570543a2c66fab4580150935c97987d4ccc
mhoong <mhoong2@yahoo.com>
2022-12-16 10:48:32 -0700

    Michael about me page

---
+++ react-about-us/src/pages/Michael/index.js
@@ -0,0 +1,28 @@
+
+const Michael = () => {
+    return (
+        <div>
+            <h1>Michael Hoong</h1>
+            <img src='https://i.imgur.com/WgIfSYW.jpeg' />
+            <h3>Bio</h3>
+            <p>Decade of experience working in the restaurant industry</p>
+            <h3>Bio</h3>
+            <ul>
+                <ls>Snowboarding</ls>
+                <ls>Cars</ls>
+                <ls>Video Games</ls>
+            </ul>
+            <h3>Professional Skills</h3>
+            <ul>
+                <ls>HTML</ls>
+                <ls>CSS</ls>
+                <ls>JavaScript</ls>
+                <ls>MongoDB</ls>
+                <ls>EJS</ls>
+                <ls>React</ls>
+            </ul>
+        </div>
+    )
+}
+
+export default Michael
