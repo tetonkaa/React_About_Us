118fb2cead73065236e0a58a78bab1f663f2d715
Angel <angel.l.puente@gmail.com>
2022-12-16 11:44:59 -0600

    added angel page

---
+++ react-about-us/src/pages/Angel/index.js
@@ -0,0 +1,10 @@
+import './main.css'
+
+export default function Angel () {
+    return(
+        <div>
+            <h1 class='Angel'>Angel</h1>
+            <img src="https://i.pinimg.com/736x/1c/e0/e4/1ce0e4e06aa6b53155c58dfc42b4b8d6.jpg"/>
+        </div>
+    )
+}
